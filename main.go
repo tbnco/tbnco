440095ffe9f7e919000fd09bdd530faafd43a672
Matthias Preu <5973515+mpreu@users.noreply.github.com>
2022-09-09 03:16:17 +0200

    Add scaffolding for network CRD
    Namespaced-scoped, as network configurations will be established per
    workload instance. This allows easier identification which configs
    relate to a workload and handling of deletion operations
    can be simplified. Can be changed later if use-cases should arise.
    
    Some general changes for linting, etc.

--- main.go
+++ main.go
@@ -18,6 +18,9 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	networkv1alpha1 "github.com/tbnco/tbnco/apis/network/v1alpha1"
+	networkcontrollers "github.com/tbnco/tbnco/controllers/network"
 	//+kubebuilder:scaffold:imports
 )
 
@@ -29,6 +32,7 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
+	utilruntime.Must(networkv1alpha1.AddToScheme(scheme))
 	//+kubebuilder:scaffold:scheme
 }
 
@@ -62,6 +66,13 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&networkcontrollers.NetworkReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Network")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
